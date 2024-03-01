72c4df5c81c62d53c4958e8c864b78ed1401d7e3
FlowgeniusJB <146901850+jbeduze@users.noreply.github.com>
2024-03-01 00:47:13 -0600

    Update pagesetup.py

--- config/pagesetup.py
+++ config/pagesetup.py
@@ -45,7 +45,7 @@ def get_title_no_divider(varPageNumber):
         css_styles=["""
             {
                 border: 1px solid rgba(115, 0, 0, 1);
-                background-color: green;
+                background-color: rgba(115, 0, 0, .8);
                 border-radius: 0.1rem;
                 padding: calc(0.2em - 10px);
             }
